4133bf42a2ceda47bf319ffb391d6bc110ebc981
Uzoma <up.uisebor@gmail.com>
2019-05-07 18:21:33 -0400

    Addeded files from Developer Console

---
+++ force-app/main/default/aura/campingList/campingListController.js
@@ -0,0 +1,45 @@
+({
+    clickCreateItem : function(component, event, helper) {
+        var validExpense = component.find('expenseform').reduce(function (validSoFar, inputCmp) {
+            // Displays error messages for invalid fields
+            inputCmp.showHelpMessageIfInvalid();
+            return validSoFar && inputCmp.get('v.validity').valid;
+        }, true);
+        // If we pass error checking, do some real work
+        if(validExpense){
+
+            var newCampingItem = component.get("v.newItem");
+            //helper.createCamping(component,newCampingItem);
+            var campings = component.get("v.items");
+            var item = JSON.parse(JSON.stringify(newCampingItem));
+            
+            campings.push(item);
+            
+            component.set("v.items",campings);
+            component.set("v.newItem",{ 'sobjectType': 'Camping_Item__c','Name': '','Quantity__c': 0,
+                                       'Price__c': 0,'Packed__c': false });
+        }
+        
+        helper.createItem(component, campings);
+        
+        
+    },
+    
+        // Load expenses from Salesforce
+    doInit: function(component, event, helper) {
+        // Create the action
+        var action = component.get("c.getItems");
+        // Add callback behavior for when response is received
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.items", response.getReturnValue());
+            }
+            else {
+                console.log("Failed with state: " + state);
+            }
+        });
+        // Send action off to be executed
+        $A.enqueueAction(action);
+    },
+})
